bbed5b1cf5d23c0fbfe9359c953308c9a675665a
Joao Victor Zarate <zzarate@users.noreply.github.com>
2019-05-27 22:38:25 -0400

    Codigo reescrito para melhor compreensao

--- src/application/InterfaceController.java
+++ src/application/InterfaceController.java
@@ -2,31 +2,37 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.EventHandler;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.event.ActionEvent;
 
 public class InterfaceController implements Initializable {
 
 	// Variaveis de seleção de função - lapis selecionado por padrão
-	boolean lapisSelect = true;
+	boolean lapisSelect = false;
 	boolean desfazerSelect = false;
 	boolean refazerSelect = false;
 	boolean preencheSelect = false;
 	boolean quadradoSelect = false;
-	boolean circsSelect = false;
+	boolean circSelect = false;
 	boolean linhaSelect = false;
 	boolean textSelect = false;
 	boolean borrachaSelect = false;
+
+	GraphicsContext dLapis;
+	GraphicsContext dLinha;
+	GraphicsContext apagar;
+	GraphicsContext dQuadrado;
+	GraphicsContext dCirculo;
 
 	@FXML
 	private ColorPicker escolheCor;
@@ -37,42 +43,131 @@ public class InterfaceController implements Initializable {
 	@FXML
 	private Canvas canvas;
 
-	GraphicsContext desenhoLapis;
-	GraphicsContext desenharLinha;
-	
-
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
-		desenhoLapis = canvas.getGraphicsContext2D();
-		desenharLinha = canvas.getGraphicsContext2D();
+		
+		dLapis = canvas.getGraphicsContext2D();
+		apagar = canvas.getGraphicsContext2D();
+		dQuadrado = canvas.getGraphicsContext2D();
+		dCirculo = canvas.getGraphicsContext2D();
+		dLinha = canvas.getGraphicsContext2D();
+		
+		Line linha = new Line();
+		Rectangle ret = new Rectangle();
+		Circle circ = new Circle();
+		
+		
+		//efefes
+		canvas.setOnMousePressed(e -> {
 
+			if (lapisSelect) {
+				double tamanho = Double.parseDouble(tamanhoSelect.getText());
+				double x = e.getX() - tamanho/2;
+				double y = e.getY() - tamanho/2;
+				dLapis.setFill(escolheCor.getValue());
+				dLapis.setStroke(escolheCor.getValue());
+				dLapis.fillRoundRect(x, y, tamanho, tamanho, tamanho, tamanho);
+			} else {
+				if (borrachaSelect) {
+					apagar.setFill(Color.WHITE);
+					apagar.setStroke(Color.WHITE);
+					apagar.setLineWidth(Double.parseDouble(tamanhoSelect.getText()));
+					apagar.beginPath();
+					apagar.lineTo(e.getX(), e.getY());
+				} else {
+					if (quadradoSelect) {
+						dQuadrado.setStroke(escolheCor.getValue());
+						ret.setX(e.getX());
+						ret.setY(e.getY());
+					} else {
+						if (linhaSelect) {
+							dLinha.setFill(escolheCor.getValue());
+							linha.setStartX(e.getX());
+							linha.setStartY(e.getY());
+						} else {
+							if (circSelect) {
+								dCirculo.setFill(escolheCor.getValue());
+								circ.setCenterX(e.getX());
+								circ.setCenterY(e.getY());
+							}
+						}
+					}
+				}
+			}
+		});
+		
+		//efefefw
 		canvas.setOnMouseDragged(e -> {
+
+			if (lapisSelect) {
+				double tamanho = Double.parseDouble(tamanhoSelect.getText());
+				double x = e.getX() - tamanho/2;
+				double y = e.getY() - tamanho/2;
+				dLapis.setFill(escolheCor.getValue());
+				dLapis.setStroke(escolheCor.getValue());
+				dLapis.fillRoundRect(x, y, tamanho, tamanho, tamanho, tamanho);
+			} else {
+				if (borrachaSelect) {
+					apagar.lineTo(e.getX(), e.getY());
+					apagar.stroke();
+				}
+			}
+
+		});
+		
+		//fefesfs
+		canvas.setOnMouseReleased(e -> {
+			/*
 			double tamanho = Double.parseDouble(tamanhoSelect.getText());
 			double x = e.getX() - tamanho / 2;
 			double y = e.getY() - tamanho / 2;
+			*/
 
-			if (lapisSelect && !tamanhoSelect.getText().isEmpty()) {
-				desenhoLapis.setFill(escolheCor.getValue());
-				desenhoLapis.fillRoundRect(x, y, tamanho, tamanho, tamanho, tamanho);
+			if (lapisSelect) {
+				double tamanho = Double.parseDouble(tamanhoSelect.getText());
+				double x = e.getX() - tamanho/2;
+				double y = e.getY() - tamanho/2;
+				dLapis.setFill(escolheCor.getValue());
+				dLapis.setStroke(escolheCor.getValue());
+				dLapis.fillRoundRect(x, y, tamanho, tamanho, tamanho, tamanho);
+			} else {
+				if (borrachaSelect) {
+					apagar.lineTo(e.getX(), e.getY());
+					apagar.stroke();
+					apagar.closePath();
+				}
 			}
 		});
 		
 	}
 	
+	//Metodos para desenhar
 	
-	
-
 	@FXML
 	public void lapisSelect(ActionEvent e) {
 		lapisSelect = true;
 	}
-	
+
 	@FXML
-	public void linhaSelect (ActionEvent e) {
+	public void linhaSelect(ActionEvent e) {
 		linhaSelect = true;
 	}
 	
+	@FXML
+	public void borrachaSelect(ActionEvent e) {
+		borrachaSelect = true;
+	}
+	
+	@FXML
+	public void quadradoSelect(ActionEvent e) {
+		quadradoSelect = true;
+	}
+	
+	@FXML
+	public void circsSelect(ActionEvent e) {
+		circSelect = true;
+	}
 
 	// Metodo para salvar
 	public void salvar() {
@@ -91,25 +186,10 @@ public class InterfaceController implements Initializable {
 
 	}
 
-	public void quadradoSelect() {
-
-	}
-
-	public void circsSelect() {
-
-	}
-
-	public void linhaSelect() {
-
-	}
-
 	public void textSelect() {
 
 	}
-
-	public void borrachaSelect() {
-
-	}
+	
 
 	// Metodo para sair da aplicação
 	public void exit() {
