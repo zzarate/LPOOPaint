b2d001ccf830ba25460aba7c07104ba9320fc802
Joao Victor Zarate <zzarate@users.noreply.github.com>
2019-06-05 16:27:08 -0400

    Adcionado incone a aplicação

--- src/application/Paint.java
+++ src/application/Paint.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -18,7 +19,9 @@ public class Paint extends Application {
 		Parent root= FXMLLoader.load(getClass().getResource("PaintInterface.fxml"));
 		Scene scene= new Scene (root);
 		
-		stage.setTitle("Paint");
+		stage.setTitle("Paint"); // Define o titulo da aplicação
+		Image icone = new Image(getClass().getResourceAsStream("/Icons/icon.png")); // Define a localização do icone
+		stage.getIcons().add(icone); // Adciona o icone a aplicação
 		stage.setScene(scene);
 		stage.show();
 	}
